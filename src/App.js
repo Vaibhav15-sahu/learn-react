0031675dc91065c9f04cbcce1b3389a7e3dbcde4
Vaibhav15-sahu <vaibhavsahu0115@gmail.com>
2024-02-02 14:32:33 +0530

    learn props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import User from './User';
+import React, { useState } from 'react';
 // import Counter from './Counter';
 
 function App() {
+  const [name, setName] = useState("");
+
+  function handleInputChange(event) {
+    setName(event.target.value);
+  }
   return (
     <>
       <div className="App">
-        <User />
+        <input type='text' onChange={handleInputChange} placeholder='enter your name'></input>
+        <User Name={name}/>
         {/* <Counter /> */}
       </div>
     </>
--- src/User.js
+++ src/User.js
@@ -1,16 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-function User() {
-    const [name, setName] = useState("");
-    
-    function handleInputChange(event){
-        setName(event.target.value) ;
-    }
-
+function User(props) {
+    console.log(props);
     return (
         <>
-            <input type='text' onChange={ handleInputChange } placeholder='enter your name'></input>
-            <h1>Hello {name} ... !</h1>
+            <h1>Hello {props.Name} ... !</h1>
         </>
     )
 }
