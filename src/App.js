b506a0a2076a794acedcf77f569b8134f494dc89
Vaibhav15-sahu <vaibhavsahu0115@gmail.com>
2024-02-02 14:41:04 +0530

    learn toggle and condition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
 import User from './User';
 import React, { useState } from 'react';
-// import Counter from './Counter';
+import Counter from './Counter';
 
 function App() {
   const [name, setName] = useState("");
+  const [toggleBtn, setToggleBtn] = useState(false);   
 
   function handleInputChange(event) {
     setName(event.target.value);
   }
+
   return (
     <>
       <div className="App">
-        <input type='text' onChange={handleInputChange} placeholder='enter your name'></input>
-        <User Name={name}/>
-        {/* <Counter /> */}
+        {!toggleBtn ?
+        <div>
+          <input type='text' onChange={handleInputChange} placeholder='enter your name'></input>
+          <User Name={name}/>
+        </div> 
+        :
+        null}
+      </div>
+      <div className="toggle-button">
+        <button onClick={() => {setToggleBtn(!toggleBtn)}} > Counter </button>
+      </div>
+      <div className="App">
+        { toggleBtn ? <Counter /> : null  }
       </div>
     </>
   );
